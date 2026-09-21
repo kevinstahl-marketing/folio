74c5bcc078e079b544f6d7ae48ec808d5def4489
Kevin Stahl <kevin@thestahlmarketing.com>
2026-09-21 18:47:40 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import "./globals.css";
 
 /* =========================================================
@@ -179,6 +182,9 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <SpeedInsights/>
+        
+        < Analytics />
       </body>
     </html>
   );
